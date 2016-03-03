3d4c5defe206d5c4c341dc09b13a3a9bad842654
franalvacad <franalvacad@gmail.com>
2016-03-03 11:19:53 -0800

    fixed up the header

--- finalProject/Header1.h
+++ finalProject/Header1.h
@@ -8,12 +8,13 @@ private:
 public:
 	display();
 
-	accsessors
-	string Getline(){
-		tempstr = base::getTitle() + " " + base::getPublisher() + " " + base::getAuthor() +
-		" " + base::getISBN() + " " + to_string(base::getCost()) + " " + to_string(base::getMSRP()) + " " + to_string(base::getQty()) +
-		" " + to_string(base::Serial()) + " " + to_string(base::getYear());
-		return tempstring;
+//	accsessors
+	string Getline()
+	{
+		tempstr = baseClass::getTitle() + " " + baseClass::getPublisher() + " " + baseClass::getAuthor() +
+		" " + baseClass::getISBN() + " " + to_string(baseClass::getCost()) + " " + to_string(baseClass::getMSRP()) + " " + to_string(baseClass::getQty()) +
+		" " + to_string(baseClass::Serial()) + " " + to_string(baseClass::getYear());
+		return tempstr;
 		string Getlist()
-		}
+	}
 };
