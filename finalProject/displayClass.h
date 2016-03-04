07e298ed520b1c8ecfdb31b16ded91c1542ad406
flotazani <flotazani@mail.ru>
2016-03-04 14:34:24 -0800

    add display

---
+++ finalProject/displayClass.h
@@ -0,0 +1,13 @@
+#include "baseClass.h"
+#include "iostream"
+#include "string"
+using namespace std;
+class display : public baseClass{
+private:
+	string tempstr;
+public:
+	display();
+
+//	accsessors afsdadg
+	string Getline();
+};
