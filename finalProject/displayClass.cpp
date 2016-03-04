de8603c30448437c9662574c01c7715640659ffa
flotazani <flotazani@mail.ru>
2016-03-04 14:17:26 -0800

    created dispalyclass ccp

---
+++ finalProject/displayClass.cpp
@@ -0,0 +1,12 @@
+#include"displayClass.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+string display::Getline()
+{
+	tempstr = baseClass::getTitle() + " " + baseClass::getPublisher() + " " + baseClass::getAuthor() +
+		" " + baseClass::getISBN() + " " + to_string(baseClass::getCost()) + " " + to_string(baseClass::getMSRP()) + " " + to_string(baseClass::getQty()) +
+		" " + to_string(baseClass::getSerial()) + " " + to_string(baseClass::getYear());
+	return tempstr; }
