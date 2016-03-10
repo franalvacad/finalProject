17e09b84898ef5e389a38bd366827ed048807e82
franalvacad <franalvacad@gmail.com>
2016-03-10 12:23:14 -0800

    added in

---
+++ finalProject/inventory.cpp
@@ -0,0 +1,11 @@
+#include "inventory.h"
+
+
+inventory::inventory()
+{
+}
+
+
+inventory::~inventory()
+{
+}
---
+++ finalProject/inventory.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "displayClass.h"
+class inventory :
+	public display
+{
+public:
+	inventory();
+	~inventory();
+};
+
