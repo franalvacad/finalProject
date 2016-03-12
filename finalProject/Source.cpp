cb07f60a822ba38727f7384a5b640ff9561a9224
franalvacad <franalvacad@gmail.com>
2016-03-12 12:30:21 -0800

    commented

--- finalProject/Source.cpp
+++ finalProject/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "inventory.cpp"
+#include <time.h>
 using namespace std;
 
 int main()
 {
+
 	inventory test;
 	display test2;
 	int size = test.getSizeLine(), srch;
@@ -15,6 +17,10 @@ int main()
 
 	test2.getList();
 
+	time_t c = time(NULL);
+	char *h = ctime(&c);
+
+	cout << h;
 
 	system("pause>nul");
 	return 0;
--- finalProject/displayClass.cpp
+++ finalProject/displayClass.cpp
@@ -100,22 +100,23 @@ string display::getCostlist()
 			}
 		}
 	}
+	return "hs";
 }
-string display::getQTYlist()
-{
-	int max = 0; // depeds from list (qty,cost,age) sort displaylistis
-	int x = baseClass::getSizeLine();
-	bool *chk = new bool[x];
-
-	for (int i = 0; i < x; i++){
-		for (int j = 0; j < x; j++){
-			if (max <= baseClass::getQty(j) && chk[j] == false){
-				max = j;
-				chk[j] = true;
-				cout << "(T__T)";
-				max = 0;
-
-			}
-		}
-	}
-}
+//string display::getQTYlist()
+//{
+//	int max = 0; // depeds from list (qty,cost,age) sort displaylistis
+//	int x = baseClass::getSizeLine();
+//	bool *chk = new bool[x];
+//
+//	for (int i = 0; i < x; i++){
+//		for (int j = 0; j < x; j++){
+//			if (max <= baseClass::getQty(j) && chk[j] == false){
+//				max = j;
+//				chk[j] = true;
+//				cout << "(T__T)";
+//				max = 0;
+//
+//			}
+//		}
+//	}
+//}
