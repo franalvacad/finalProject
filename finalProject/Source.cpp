30eb2272046e4921cac1f05ab6cce80d5e64ff67
flotazani <flotzani@mail.ru>
2016-03-11 20:58:47 -0800

    finished Agelist, QTYlist and Costlist funcitons in display class

--- finalProject/Source.cpp
+++ finalProject/Source.cpp
@@ -6,16 +6,16 @@ int main()
 {
 	inventory test;
 	display test2;
-	int size = test.getSizeLine(), srch;
-	string results;
+	//int size = test.getSizeLine(), srch;
+	//string results;
 
 
 
 	//cout << "term: ";
 
-	results = test.getTitle(4);
+	//results = test.getTitle(4);
 
-	test2.getList();
+	test2.getAgelist();
 
 
 	system("pause>nul");
--- finalProject/displayClass.cpp
+++ finalProject/displayClass.cpp
@@ -22,7 +22,7 @@ void display::getList()
 	{
 		cout << setw(4) << left << baseClass::getSerial(i) << setw(35) << left << baseClass::getTitle(i) << setw(35) << left << baseClass::getPublisher(i) << setw(25) 
 			<< left << baseClass::getAuthor(i) << setw(15) << left << baseClass::getISBN(i) << setw(8) << left << baseClass::getCost(i) << setw(8) << left << baseClass::getMSRP(i) 
-			<< setw(5) << left << baseClass::getQty(i) << setw(10) << left << baseClass::getType(i) << endl;
+			<< setw(5) << left << baseClass::getQty(i) << setw(10) << left << baseClass::getType(i) << baseClass::getDate(i) << endl;
 	}
 }
 
@@ -64,62 +64,74 @@ string display::getRetaillist()
 
 void display::getQTYlist()
 {
-	int j = 0;
 	int max = 0; // depeds from list (qty,cost,age) sort displaylistis
 	int maxpos = 0;
 	int x = baseClass::getSizeLine();
-	bool *chk = new bool[x];
-
+	int *chk = NULL;
+	chk = new int[25];
 	for (int i = 0; i < x; i++){
-		for (j = 0; j < x; j++){
-			if (max <= baseClass::getQty(j) && chk[j] == false){
+		chk[i] = 0;
+	}
+
+	for (int i = 0; i < x-1; i++){
+		for (int j = 0; j < x-1; j++){
+			if (max <= baseClass::getQty(j) && chk[j] == 0){
 				max = getQty(j);
 				maxpos = j;
 			}
 		}
-			chk[j] = true;
-			cout << "(T__T)";
+			chk[maxpos] = 1;
+			cout << setw(4) << left << baseClass::getSerial(maxpos) << setw(35) << left << baseClass::getTitle(maxpos) << setw(35) << left << baseClass::getPublisher(maxpos) << setw(25)
+				<< left << baseClass::getAuthor(maxpos) << setw(15) << left << baseClass::getISBN(maxpos) << setw(8) << left << baseClass::getCost(maxpos) << setw(8) << left << baseClass::getMSRP(maxpos)
+				<< setw(5) << left << baseClass::getQty(maxpos) << setw(15) << left << baseClass::getType(maxpos) << baseClass::getDate(i) << endl;
 			max = 0;
-			j = 0;
 
 	}
 	//return "test";
+	delete[] chk;
 }
 
 void display::getCostlist()
 {
-	int j = 0;
 	double max = 0; // depeds from list (qty,cost,age) sort displaylistis
 	int maxpos = 0;
 	int x = baseClass::getSizeLine();
-	bool *chk = new bool[x];
-
+	int *chk = NULL;
+	chk = new int[25];
 	for (int i = 0; i < x; i++){
-		for (j = 0; j < x; j++){
-			if (max <= baseClass::getCost(j) && chk[j] == false){
+		chk[i] = 0;
+	}
+
+	for (int i = 0; i < x-1; i++){
+		for (int j = 0; j < x-1; j++){
+			if (max <= baseClass::getCost(j) && chk[j] == 0){
 				max = getCost(j);
 				maxpos = j;
 			}
 		}
 
-		chk[j] = true;
-		cout << "(T__T)";
+		chk[maxpos] = 1;
+		cout << setw(4) << left << baseClass::getSerial(maxpos) << setw(35) << left << baseClass::getTitle(maxpos) << setw(35) << left << baseClass::getPublisher(maxpos) << setw(25)
+			<< left << baseClass::getAuthor(maxpos) << setw(15) << left << baseClass::getISBN(maxpos) << setw(8) << left << baseClass::getCost(maxpos) << setw(8) << left << baseClass::getMSRP(maxpos)
+			<< setw(5) << left << baseClass::getQty(maxpos) << setw(15) << left << baseClass::getType(maxpos) << baseClass::getDate(i) << endl;
 		max = 0;
-		j = 0;
 	}
 }
 void display::getAgelist()
 {
-	int j = 0;
 	int  maxy = 0; // depeds from list (qty,cost,age) sort displaylistis
 	int  maxm = 0;
 	int  maxd = 0;
 	int maxpos = 0;
 	int x = baseClass::getSizeLine();
-	bool *chk = new bool[x];
-
+	int *chk = NULL;
+	chk = new int[25];
 	for (int i = 0; i < x; i++){
-		for ( j = 0; j < x; j++){
+		chk[i] = 0;
+	}
+
+	for (int i = 0; i < x-1; i++){
+		for (int j = 0; j < x-1; j++){
 			if (maxy <= dateAdded[j].getYear() && maxm <= dateAdded[j].getMonth() && maxd <= dateAdded[j].getDay() && chk[j] == false){
 				maxy = dateAdded[j].getYear();
 				maxm = dateAdded[j].getMonth();
@@ -128,8 +140,10 @@ void display::getAgelist()
 
 			}
 		}
-		chk[j] = true;
-		cout << "(T__T)";
+		chk[maxpos] = 1;
+		cout << setw(4) << left << baseClass::getSerial(maxpos) << setw(35) << left << baseClass::getTitle(maxpos) << setw(35) << left << baseClass::getPublisher(maxpos) << setw(25)
+			<< left << baseClass::getAuthor(maxpos) << setw(15) << left << baseClass::getISBN(maxpos) << setw(8) << left << baseClass::getCost(maxpos) << setw(8) << left << baseClass::getMSRP(maxpos)
+			<< setw(5) << left << baseClass::getQty(maxpos) << setw(15) << left << baseClass::getType(maxpos) << baseClass::getDate(i) << endl;
 		maxy = 0;
 		maxm = 0;
 		maxd = 0;	
