48d06ec38c3dbc4783c0a1a396ba423e4c88e6e8
agbares <antonio.g.bares@gmail.com>
2016-03-15 14:55:25 -0700

    Cashier

--- finalProject/Cashier.cpp
+++ finalProject/Cashier.cpp
@@ -18,14 +18,17 @@ void Cashier::addToCart()
 	{
 		cout << "The serial " << serial << " cannot be found." << endl;
 	}
+	else
+	{
 
+	}
 
 }
 
 int Cashier::findBook(int s)
 {
 	int first = 0,						// First array element
-		last = numElems - 1,			// Last array element
+		last = getSizeLine() - 1,			// Last array element
 		middle,							// Midpoint of search
 		position = -1;					// Position of search value
 	bool found = false;					// Flag
@@ -33,12 +36,12 @@ int Cashier::findBook(int s)
 	while (!found && first <= last)
 	{
 		middle = (first + last) / 2;	// Calculate midpoint
-		if (array[middle] == value)		// If value is found at mid
+		if (getSerial(middle) == s)		// If value is found at mid
 		{
 			found = true;
 			position = middle;
 		}
-		else if (array[middle] > value)	// If value is in lower half
+		else if (getSerial(middle) > s)	// If value is in lower half
 			last = middle - 1;
 		else
 			first = middle + 1;			// If value is in upper half
