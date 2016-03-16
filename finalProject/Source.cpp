172c629155ed7e928272e084a4e36d2c0139642b
franalvacad <franalvacad@gmail.com>
2016-03-15 23:27:02 -0700

    sytems

--- finalProject/Source.cpp
+++ finalProject/Source.cpp
@@ -47,20 +47,89 @@ void inventTest()
 
 }
 
+void cashMod()
+{
+	Cashier alpha;
+	int inp = 0;
+	cout << "***********************************************************************************************************" << endl;
+	cout << "*                                            CASHIER MODULE                                               *" << endl;
+	cout << "***********************************************************************************************************" << endl << endl;
+
+	cout << "Enter Item Code: ";
+	cin >> inp;
+
+	int i = alpha.findBook(inp);
+	int qty = 0;
+
+	//cout system date over here
+	cout << setw(9) << left << "Item Code" << setw(35) << left << "Title" << setw(15) << left << "ISBN" << setw(8)
+		<< left << "MSRP($)" << setw(8) << left << "Qty" << setw(8) << left << "Total($)" << endl;
+	cout << "------------------------------------------------------------------------------------------------------------------------------" << endl;
+	alpha.getListCustom(i);
+	cin >> qty;
+
+}
+
+void invMod()
+{
+	cout << "***********************************************************************************************************" << endl;
+	cout << "*                                     INVENTORY DATABASE MODULE                                           *" << endl;
+	cout << "***********************************************************************************************************" << endl << endl;
+}
 
 
+void repMod()
+{
+	display alpha;
+	int choice = 0, type = 0;
 
+	cout << "***********************************************************************************************************" << endl;
+	cout << "*                                           REPORT MODULE                                                 *" << endl;
+	cout << "***********************************************************************************************************" << endl << endl;
+
+	cout << "No. Report Name" << endl;
+	cout << "------------------------------" << endl;
+	cout << " 1: Inventory List" << endl;
+	cout << " 2: Inventory Wholesale Value" << endl;
+	cout << " 3: Inventory Retail Value" << endl;
+	cout << " 4: List by Quantity" << endl;
+	cout << " 5: List by Cost" << endl;
+	cout << " 6: List by Age" << endl;
+	cout << " 9: RETURN" << endl << endl;
+	cout << "Input choice: ";
+	cin >> type;
+
+	if (type == 1)
+		alpha.getList();
+	else if (type == 2)
+		alpha.getWholelist();
+	else if (type == 3)
+		alpha.getRetaillist();
+	else if (type == 4)
+		alpha.getQTYlist();
+	else if (type == 5)
+		alpha.getCostlist();
+	else if (type == 6)
+		alpha.getAgelist();
+	else if (type == 9);
+	//return to main
+	else cout << "Invalid Input." << endl;
+
+	cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+	choice = 0;
+	type = 0;
+}
 
 int main()
 {
-	Cashier alpha;
-	int choice = 0, type = 0;
-
-	try{
+	int choice;
+	
+	try 
+	{
 		do
 		{
 			//Menu displayed for user to make a choice
-
+	
 			cout << "***********************************************************************************************************" << endl;
 			cout << "*                                          SERENDIPITY BOOKSELLERS                                        *" << endl;
 			cout << "***********************************************************************************************************" << endl << endl;
@@ -72,79 +141,27 @@ int main()
 			cout << " 9: Exit" << endl << endl;
 			cout << "Input choice number: ";
 			cin >> choice;
-
 			if (choice == 1)
 			{
-				int inp = 0;
-				cout << "***********************************************************************************************************" << endl;
-				cout << "*                                            CASHIER MODULE                                               *" << endl;
-				cout << "***********************************************************************************************************" << endl << endl;
-
-				cout << "Enter Item Code: ";
-				cin >> inp;
-
-				int i = alpha.findBook(inp);
-				int qty = 0;
-
-				//cout system date over here
-				cout << setw(9) << left << "Item Code" << setw(35) << left << "Title" << setw(15) << left << "ISBN" << setw(8)
-					<< left << "MSRP($)" << setw(8) << left << "Qty" << setw(8) << left << "Total($)" << endl;
-				cout << "------------------------------------------------------------------------------------------------------------------------------" << endl;
-				alpha.getListCustom(i);
-				cin >> qty;
-
-
+				system("cls");
+				cashMod();
 			}
 			else if (choice == 2)
 			{
-				cout << "***********************************************************************************************************" << endl;
-				cout << "*                                     INVENTORY DATABASE MODULE                                           *" << endl;
-				cout << "***********************************************************************************************************" << endl << endl;
+				system("cls");
+				invMod();
 			}
 			else if (choice == 3)
 			{
-
-				cout << "***********************************************************************************************************" << endl;
-				cout << "*                                           REPORT MODULE                                                 *" << endl;
-				cout << "***********************************************************************************************************" << endl << endl;
-
-				cout << "No. Report Name" << endl;
-				cout << "------------------------------" << endl;
-				cout << " 1: Inventory List" << endl;
-				cout << " 2: Inventory Wholesale Value" << endl;
-				cout << " 3: Inventory Retail Value" << endl;
-				cout << " 4: List by Quantity" << endl;
-				cout << " 5: List by Cost" << endl;
-				cout << " 6: List by Age" << endl;
-				cout << " 9: RETURN" << endl << endl;
-				cout << "Input choice: ";
-				cin >> type;
-
-				if (type == 1)
-					alpha.getList();
-				else if (type == 2)
-					alpha.getWholelist();
-				else if (type == 3)
-					alpha.getRetaillist();
-				else if (type == 4)
-					alpha.getQTYlist();
-				else if (type == 5)
-					alpha.getCostlist();
-				else if (type == 6)
-					alpha.getAgelist();
-				else if (type == 9);
-				//return to main
-				else cout << "Invalid Input." << endl;
-
-				cout << "-----------------------------------------------------------------------------------------------------------" << endl;
-				choice = 0;
-				type = 0;
+				system("cls");
+				repMod();
 			}
 			
 		} while (choice != 9); //loop to make sure program does not terminate unless user exits
 
-		}
-		catch (char *extstr){ cout << extstr; }
-		system("pause>nul");
-		return 0;
+	}
+	catch (char *extstr){ cout << extstr; }
+	
+	system("pause>nul");
+	return 0;
 }
