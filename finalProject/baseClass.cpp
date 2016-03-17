c109b14d6f33884b9716de3e67f12223efc8cfff
haroonshayat <haroon.s.hayat@gmail.com>
2016-03-17 00:22:53 -0700

    sa

--- finalProject/baseClass.cpp
+++ finalProject/baseClass.cpp
@@ -101,7 +101,7 @@ void baseClass::setType(string x, int c)
 
 void baseClass::fileMod(int x) //Essentially copies whatever is in array to the end of file. Var x is max value
 {
-	Date time;
+	//Date time;
 	mainData.close();
 	ofstream test;
 	test.open("serial.txt");
